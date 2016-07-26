c363470e65858c7ab4ae8348722d1d226e42dddf
AIbe <nwannadaibe@gmail.com>
2016-07-26 23:23:39 +0000

    first attempt to add my site to the url

---
+++ bootstrap-3.3.6-dist/js/madLibsJS.js
@@ -0,0 +1,35 @@
+    function insertPhrase (){
+        var noun1Input = document.getElementById("noun1").value;
+        var messageBox = document.getElementById("typedNoun1");
+        messageBox.innerHTML = noun1Input;
+   
+        var place1Input = document.getElementById("place1").value;
+        var messageBox = document.getElementById("typedPlace1");
+        messageBox.innerHTML = place1Input;
+    
+        
+        var verb1Input = document.getElementById("verb1").value;
+        var messageBox = document.getElementById("typedVerb1");
+        messageBox.innerHTML = verb1Input;
+    
+
+        var pluralInput = document.getElementById("plural").value;
+        var messageBox = document.getElementById("typedPlural");
+        messageBox.innerHTML = pluralInput;
+
+        var weatherInput = document.getElementById("weatherAdj").value;
+        var messageBox = document.getElementById("typedWeather");
+        messageBox.innerHTML = weatherInput;
+
+        var phraseInput = document.getElementById("exclamatoryPhrase").value;
+        var messageBox = document.getElementById("typedPhrase");
+        messageBox.innerHTML = phraseInput;
+
+        var noun1Input = document.getElementById("noun1").value;
+        var messageBox = document.getElementById("typedNoun12");
+        messageBox.innerHTML = noun1Input;
+    
+    }
+
+
+ 
