ea21d7ed80163028d4ed018cd03aefb45284f8a8
AIbe <nwannadaibe@gmail.com>
2016-08-02 01:06:29 +0000

    fixed replaceWith

--- js/ibeMods.js
+++ js/ibeMods.js
@@ -3,6 +3,7 @@
    $(document).ready(function(){
         $(".likeWatch2").click(function(){    
             $("h3").replaceWith("In my spare time, I like to watch strange and unusual movies and tv shows with my bud to take my mind off of life's stresses.");
+            $("h3").fadeOut("slow");
     
     });
 });
